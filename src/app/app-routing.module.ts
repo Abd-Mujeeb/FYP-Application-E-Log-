662d5b361060a550a3aa86ce64c7e3053a176740
Abd-Mujeeb <nws06_01098750@pb.edu.bn>
2019-08-11 14:02:51 +0800

    merge change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  { path: 'uploadpage', loadChildren: './uploadpage/uploadpage.module#UploadpagePageModule' },
-  { path: 'notification', loadChildren: './notification/notification.module#NotificationPageModule' },
-
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
 ];
 
 @NgModule({
