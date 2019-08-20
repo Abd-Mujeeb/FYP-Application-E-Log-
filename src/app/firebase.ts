494c76379d50ad836e19f62a91e43d74379a546c
Aiman <nws06_01083347@pb.edu.bn>
2019-08-20 15:58:16 +0800

    add firebase config

---
+++ src/app/firebase.ts
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyDFNM5AsLEAoYQhtnZ7XYRfMZWrvbgdZ0Q",
+  authDomain: "e-log-eab02.firebaseapp.com",
+  databaseURL: "https://e-log-eab02.firebaseio.com",
+  projectId: "e-log-eab02",
+  storageBucket: "e-log-eab02.appspot.com",
+  messagingSenderId: "950153231955",
+  appId: "1:950153231955:web:3f1547f346a26aa3"
+};
+  
+    export default config
