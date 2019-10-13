faf521d81370c4aa9044a0dab04474aea2e769be
hanayossli <nws06_01086703@pb.edu.bn>
2019-10-13 12:38:11 +0800

    gc page

--- src/app/pages/home-gc/home-gc.page.ts
+++ src/app/pages/home-gc/home-gc.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from 'src/app/services/user/student.service';
 
 @Component({
   selector: 'app-home-gc',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeGcPage implements OnInit {
 
-  constructor() { }
+  public userProfile: any[];
+  public loadeduserProfile: any [];
+
+  constructor(
+    private studentService: StudentService){}
 
   ngOnInit() {
+    this.studentService.read_student().subscribe(data => {
+ 
+      this.userProfile = data.map(e => {
+           return {
+          id: e.payload.doc.id,
+          isEdit: false,
+          name: e.payload.doc.data()['name'],
+          email: e.payload.doc.data()['email'],
+          school_dept: e.payload.doc.data()['school_dept'],
+          group_code: e.payload.doc.data()['group_code'],
+          student_id: e.payload.doc.data()['student_id']
+
+        };
+      })
+      console.log(this.userProfile);
+   this.loadeduserProfile = this.userProfile;
+  
+    });
+
+  }
+
+  initializeItems(): void {
+    this.userProfile = this.loadeduserProfile;
+  }
+
+  filterList(evt){
+    this.initializeItems();
+    const searchTerm = evt.srcElement.value;
+
+    if (!searchTerm){
+      return;
+    }
+
+    this.userProfile = this.userProfile.filter(currentlist => {
+      if (currentlist.name, currentlist.email && searchTerm){
+        if (currentlist.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1 ||
+        currentlist.email.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1){
+          return true;
+        }
+        return false;
+      }
+    });
   }
 
 }
