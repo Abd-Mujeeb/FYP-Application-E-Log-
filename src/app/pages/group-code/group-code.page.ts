8cb40c23ef8961e42cc327be1c3f4ebce4fd6734
aimanrahim <nws06_01083347@pb.edu.bn>
2019-10-19 22:34:18 +0800

    filter for student details admin

--- src/app/pages/group-code/group-code.page.ts
+++ src/app/pages/group-code/group-code.page.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+import { switchMap } from 'rxjs/operators';
+import {  BehaviorSubject, combineLatest, Observable } from 'rxjs';
+
+
+
+
+export interface Item {
+  displayName: string;
+  group_code: string;
+}
 
 @Component({
   selector: 'app-group-code',
   templateUrl: './group-code.page.html',
   styleUrls: ['./group-code.page.scss'],
 })
-export class GroupCodePage implements OnInit {
+export class GroupCodePage {
 
-  constructor() { }
+  items$: Observable<Item[]>;
+  group_codeFilter$: BehaviorSubject<string|null>;
+  
+  constructor(
+    afs: AngularFirestore
+    ) 
+    {
 
-  ngOnInit() {
+    this.group_codeFilter$ = new BehaviorSubject(null);
+
+    this.items$ = combineLatest(
+      this.group_codeFilter$,
+   
+    ).pipe(switchMap(([group_code]) => 
+      afs.collection<Item>('users', ref => {
+        let query : firebase.firestore.CollectionReference | firebase.firestore.Query = ref;
+        if (group_code) { query = query.where('group_code', '==', group_code) };
+        return query;
+      }).valueChanges()
+    )
+    );
+  }
+  filterBygroup_code(group_code: string|null) {
+    this.group_codeFilter$.next(group_code); 
   }
 
 }
