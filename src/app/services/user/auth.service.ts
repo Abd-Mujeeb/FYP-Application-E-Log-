4391c6fda1cc31eb9a3f5b8a5251a8880214d5c3
hanayossli <nws06_01086703@pb.edu.bn>
2019-11-09 19:33:40 +0800

    pbsupervisor: 'nopbsupervisor'

--- src/app/services/user/auth.service.ts
+++ src/app/services/user/auth.service.ts
@@ -113,7 +113,7 @@ export class AuthService {
         firebase
           .firestore()
           .doc(`/student/${newUserCredential.user.uid}`)
-          .set({number, displayName, name, email, school_dept, group_code, student_id, gc, company, password, role:'student', change: true});
+          .set({number, displayName, name, email, school_dept, group_code, student_id, gc, company, password, role:'student', change: true, pbsupervisor: 'nosupervisor' });
           console.log("users " + newUserCredential.user.email + " created successfully!");
           secondaryApp.auth().signOut();
       }).catch(error => {
