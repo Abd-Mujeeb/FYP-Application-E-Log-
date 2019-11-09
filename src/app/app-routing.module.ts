c71084ffdf69f0a96d3e6098fa4354ff0e92535b
hanayossli <nws06_01086703@pb.edu.bn>
2019-11-09 18:46:39 +0800

    delete import page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -331,7 +331,6 @@ const routes: Routes = [
   { path: 'changepw-i', loadChildren: './pages/changepw-i/changepw-i.module#ChangepwIPageModule' },
   { path: 'changepw-gc', loadChildren: './pages/changepw-gc/changepw-gc.module#ChangepwGcPageModule' },
   { path: 'changepw-student', loadChildren: './pages/changepw-student/changepw-student.module#ChangepwStudentPageModule' },
-  { path: 'import', loadChildren: './pages/import/import.module#ImportPageModule' },
   { path: 'image-modal', loadChildren: './pages/image-modal/image-modal.module#ImageModalPageModule' },
   { path: 'register-pbsupervisor', loadChildren: './pages/register-pbsupervisor/register-pbsupervisor.module#RegisterPbsupervisorPageModule' },
   { path: 'register-gc', loadChildren: './pages/register-gc/register-gc.module#RegisterGcPageModule' },
