e7fa6c33bd33d1806733fed7faa458d960993a91
Abd-Mujeeb <nws06_01098750@pb.edu.bn>
2019-11-15 14:14:51 +0800

    make page to view specific attenddance

---
+++ src/app/gc-studentlist-modal/gc-studentlist-modal.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gc-studentlist-modal',
+  templateUrl: './gc-studentlist-modal.page.html',
+  styleUrls: ['./gc-studentlist-modal.page.scss'],
+})
+export class GcStudentlistModalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
